eb710eed62335f5eafbc66ef2860ac69eac876da
tomcawthorn <tom_cawthorn@btinternet.com>
2014-08-04 15:36:49 +0100

    Exchanged update for save in todo list form partial

--- app/views/todos/partials/_form.blade.php
+++ app/views/todos/partials/_form.blade.php
@@ -1,4 +1,4 @@
 		{{ Form::label('name', 'List Title') }}
 		{{ Form::input('text', 'name') }}		
 		{{ $errors->first('name', '<small class="error">:message</small>') }}	
-		{{ Form::submit('update', array('class' => 'button')) }}
+		{{ Form::submit('Save', array('class' => 'button')) }}
--- app/views/todos/show.blade.php
+++ app/views/todos/show.blade.php
@@ -3,8 +3,10 @@
 	<div class="large-12 columns">
 		<p>{{ link_to_route('todos.index', 'Back') }}</p>
 		<h1>{{{ $list->name }}}</h1>
+
 		@foreach ($items as $item)
 			<h4>{{ link_to_route('todos.items.show', $item->content, [$list->id, $item->id]) }}</h4>
 		@endforeach
+		
 	</div>
 @stop
