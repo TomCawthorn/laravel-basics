3802e151ab26feb16acec298db718a9f60835af5
tomcawthorn <tom_cawthorn@btinternet.com>
2014-08-05 18:20:15 +0100

    tidy views

--- app/views/items/partials/_form.blade.php
+++ app/views/items/partials/_form.blade.php
@@ -1,4 +1,4 @@
-		{{ Form::label('content', 'Item') }}
-		{{ Form::input('text', 'content') }}		
-		{{ $errors->first('content', '<small class="error">:message</small>') }}	
-		{{ Form::submit('Save', array('class' => 'button')) }}
+{{ Form::label('content', 'Item') }}
+{{ Form::input('text', 'content') }}		
+{{ $errors->first('content', '<small class="error">:message</small>') }}	
+{{ Form::submit('Save', array('class' => 'button')) }}
