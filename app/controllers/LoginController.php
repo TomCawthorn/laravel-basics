ec98ff09db843ad92e193ecf445a23229dc78ae6
tomcawthorn <tom_cawthorn@btinternet.com>
2014-08-06 11:23:55 +0100

    Add storeRegister function and create group to controller

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -28,8 +28,66 @@ return View::make('register');
  
 //Register User
 public function storeRegister() {
+        // Gather Sanitized Input
+        $input = array('username' => Input::get('username'), 'email' => Input::get('email'), 'password' => Input::get('password'), 'password_confirmation' => Input::get('password_confirmation'));
  
-}
+        // Set Validation Rules
+        $rules = array('username' => 'required|min:4|max:20|unique:profile,username', 'email' => 'required|min:4|max:32|email|unique:profile,email', 'password' => 'required|min:6|confirmed', 'password_confirmation' => 'required|same:password');
+ 
+        //Run input validation
+        $v = Validator::make($input, $rules);
+ 
+        if ($v -> fails()) {
+            return Redirect::to('/register') -> withErrors($v) -> withInput(Input::except(array('password', 'password_confirmation')));
+        } else {
+ 
+            try {
+                //Pre activate user
+                $user = Sentry::register(array('email' => $input['email'], 'password' => $input['password']), true);
+                //$user = Sentry::register(array('email' => $input['email'], 'password' => $input['password']));
+ 
+                //Get the activation code & prep data for email
+                $data['activationCode'] = $user -> GetActivationCode();
+                $data['email'] = $input['email'];
+                $data['userId'] = $user -> getId();
+ 
+                //send email with link to activate.
+                /*Mail::send('emails.register_confirm', $data, function($m) use ($data) {
+                 $m -> to($data['email']) -> subject('Thanks for Registration - Support Team');
+                 });*/
+ 
+                //If no groups created then create new groups
+                try {
+                    $user_group = Sentry::findGroupById(1);
+                } catch (Cartalyst\Sentry\Groups\GroupNotFoundException $e) {
+                    $this -> createGroup('users');
+                    $this -> createGroup('admin');
+                    $user_group = Sentry::findGroupById(1);
+                }
+ 
+                $user -> addGroup($user_group);
+ 
+                $user = new Profile();
+ 
+                $user -> user_id = $data['userId'];
+                $user -> email = $data['email'];
+                $user -> username = $input['username'];
+                $user -> save();
+ 
+                //success!
+                Session::flash('success_msg', 'Thanks for sign up . Please activate your account by clicking activation link in your email');
+                return Redirect::to('/register');
+ 
+            } catch (Cartalyst\Sentry\Users\LoginRequiredException $e) {
+                Session::flash('error_msg', 'Username/Email Required.');
+                return Redirect::to('/register') -> withErrors($v) -> withInput(Input::except(array('password', 'password_confirmation')));
+            } catch (Cartalyst\Sentry\Users\UserExistsException $e) {
+                Session::flash('error_msg', 'User Already Exist.');
+                return Redirect::to('/register') -> withErrors($v) -> withInput(Input::except(array('password', 'password_confirmation')));
+            }
+ 
+        }
+    }
  
 //Show forgotpassword Form
 public function showForgotpassword() {
@@ -51,4 +109,35 @@ public function storeNewPassword() {
  
 }
  
+//Create user groups 
+public function createGroup($groupName) {
+        $input = array('newGroup' => $groupName);
+ 
+        // Set Validation Rules
+        $rules = array('newGroup' => 'required|min:4');
+ 
+        //Run input validation
+        $v = Validator::make($input, $rules);
+ 
+        if ($v -> fails()) {
+            return false;
+        } else {
+            try {
+                $group = Sentry::getGroupProvider() -> create(array('name' => $input['newGroup'], 'permissions' => array('admin' => Input::get('adminPermissions', 0), 'users' => Input::get('userPermissions', 0), ), ));
+ 
+                if ($group) {
+                    return true;
+                } else {
+                    return false;
+                }
+ 
+            } catch (Cartalyst\Sentry\Groups\NameRequiredException $e) {
+                return false;
+            } catch (Cartalyst\Sentry\Groups\GroupExistsException $e) {
+                return false;
+            }
+        }
+    }
+
+
 }
